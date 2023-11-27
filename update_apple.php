2aca314a94886cf17be2c20bca50d25b16c867f1
Ageng <agengpraba@gmail.com>
2023-11-27 17:14:21 +0700

    V5.2

--- update_apple.php
+++ update_apple.php
@@ -1,6 +1,6 @@
 <?php
 include "koneksi.php";
-
+session_start();
 if(!isset($_SESSION['role'])){
   header('Location: login_register.php');
   exit();
@@ -28,46 +28,57 @@ if(isset($_GET['edit'])){
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Insert Apples</title>
-    <link rel="stylesheet" href="css/home.css" />
-    <link rel="stylesheet" href="css/apples.css" />
-    <link rel="stylesheet" href="css/insert_apple.css" />
-  </head>
-  <body>
-    <div class="container">
-      <nav>
-        <div id="menuToggle">
-          <input type="checkbox" />
-          <span></span>
-          <span></span>
-          <span></span>
-          <ul id="menu">
-            <a href="home.php"><li>HOME</li></a>
-            <a href="profile.php"><li>PROFILE</li></a>
-            <a href="apples.php"><li>APPLES</li></a>
-            <a href="logout.php"><li id="logout">LOG OUT</li></a>
-          </ul>
-        </div>
-        <img src="images/now whit.svg" alt="logo" />
-      </nav>
-      <div class="content">
-        <img class="img-witch" src="images/witch (3).png" />
-        <div class="form-apple">
-          <form class="insert-apple" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
-            <input type="hidden" name="id_upd" value="<?php echo $edit['id'];?>">
-            <label for="">Type :</label>
-            <input type="text" name="jenis_apel" value="<?php echo $edit['jenis_apel']?>"/><br />
-            <label for="">Detail :</label>
-            <input type="text" name="detail_apel" value="<?php echo $edit['detail_apel']?>"/>
-            <input type="submit" name="update" id="" class="btn-submit" value="Update" />
-          </form>
-        </div>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Insert Apples</title>
+  <link rel="stylesheet" href="css/home.css" />
+  <link rel="stylesheet" href="css/apples.css" />
+  <link rel="stylesheet" href="css/insert_apple.css" />
+</head>
+
+<body>
+  <div class="container">
+    <nav>
+      <div id="menuToggle">
+        <input type="checkbox" />
+        <span></span>
+        <span></span>
+        <span></span>
+        <ul id="menu">
+          <a href="home.php">
+            <li>HOME</li>
+          </a>
+          <a href="profile.php">
+            <li>PROFILE</li>
+          </a>
+          <a href="apples.php">
+            <li>APPLES</li>
+          </a>
+          <a href="logout.php">
+            <li id="logout">LOG OUT</li>
+          </a>
+        </ul>
+      </div>
+      <img src="images/now whit.svg" alt="logo" />
+    </nav>
+    <div class="content">
+      <img class="img-witch" src="images/witch (3).png" />
+      <div class="form-apple">
+        <form class="insert-apple" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+          <input type="hidden" name="id_upd" value="<?php echo $edit['id'];?>">
+          <label for="">Type :</label>
+          <input type="text" name="jenis_apel" value="<?php echo $edit['jenis_apel']?>" /><br />
+          <label for="">Detail :</label>
+          <input type="text" name="detail_apel" value="<?php echo $edit['detail_apel']?>" />
+          <input type="submit" name="update" id="" class="btn-submit" value="Update" />
+        </form>
       </div>
     </div>
-  </body>
+  </div>
+</body>
+
 </html>
 <?php
 }
